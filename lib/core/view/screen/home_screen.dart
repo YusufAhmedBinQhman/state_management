45b4847d06e5374c33bab65560ef5cc303325738
Yusuf <yusuf711ahmed@gmail.com>
2024-09-22 19:55:35 +0300

    add Game container

--- lib/core/view/screen/home_screen.dart
+++ lib/core/view/screen/home_screen.dart
@@ -29,7 +29,58 @@ class HomeScreen extends StatelessWidget {
                 "${h.counter}",
                 style: TextStyle(fontSize: 50),
               ));
-            })
+            }),
+            Stack(
+              children: [
+                Container(height: 200, width: 200, color: Colors.red),
+                Consumer<HomeVM>(builder: (ctx, h, child) {
+                  return Positioned(
+                      bottom: h.topDownM.toDouble(),
+                      // top: h.topDownM.toDouble(),
+                      // right: h.righLefttM.toDouble(),
+                      left: h.righLefttM.toDouble(),
+                      child:
+                          Container(height: 50, width: 50, color: Colors.blue));
+                }),
+              ],
+            ),
+            Consumer<HomeVM>(builder: (ctx, h, child) {
+              return ElevatedButton(
+                  onPressed: () {
+                    h.topMove();
+                  },
+                  onLongPress: () {
+                    h.topMove();
+                  },
+                  child: Text("اعلى"));
+            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<HomeVM>(builder: (ctx, h, child) {
+                  return ElevatedButton(
+                      onPressed: () {
+                        h.leftMove();
+                      },
+                      child: Text("يسار"));
+                }),
+                SizedBox(width: 50),
+                Consumer<HomeVM>(builder: (ctx, h, child) {
+                  return ElevatedButton(
+                      onPressed: () {
+                        h.rightMove();
+                      },
+                      child: Text("يمين"));
+                }),
+              ],
+            ),
+            Consumer<HomeVM>(builder: (ctx, h, child) {
+              return ElevatedButton(
+                  onPressed: () {
+                    h.downMove();
+                  },
+                  child: Text("اسفل"));
+            }),
           ],
         ),
       ),
--- lib/core/view_model/HomeVM.dart
+++ lib/core/view_model/HomeVM.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
 
 class HomeVM with ChangeNotifier {
+  int topDownM = 0;
   int counter = 0;
+  int righLefttM = 0;
+
   addToCounter() {
     notifyListeners();
-    counter++;
+    counter += 6;
+  }
+
+  topMove() {
+    notifyListeners();
+    topDownM += 6;
+  }
+
+  downMove() {
+    notifyListeners();
+    topDownM -= 6;
+  }
+
+  rightMove() {
+    notifyListeners();
+    righLefttM += 6;
+  }
+
+  leftMove() {
+    notifyListeners();
+    righLefttM -= 6;
   }
 }
